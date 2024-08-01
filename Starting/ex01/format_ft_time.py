7f800ee029945721611b2de972079768ee323366
Alex Lamizana <lamizana1987@yahoo.fr>
2024-08-01 12:17:18 +0200

    push 010aout

--- Starting/ex01/format_ft_time.py
+++ Starting/ex01/format_ft_time.py
@@ -1,17 +1,20 @@
 import datetime
 import time
 
+# Stocker les couleurs d'affichages:
+red = "\033[0;31m"
+white = "\033[0m"
+
 # Obtenir le temps actuel en secondes depuis l'époque:
 second = time.time()
-
 second_str = str(second)
-print(second)
-
-print(f"Seconds since January 1, 1970: {second_str[0]},{second_str[1:4]},{second_str[5:8]},{second_str[9:11]}{second_str[12:16]} ")
-print(" or {0:.2e} in scientific notation".format(second))
 
 # Recupere la date actuel:
 current_date = datetime.datetime.now()
 
 # Formate et affiche la date:
+print(f"{red}Seconds since January 1, 1970:{white} ", end="")
+print(f"{second_str[0]},{second_str[1:4]},{second_str[4:7]},{second_str[7:15]}", end="")
+print(" or {0:.2e} in scientific notation".format(second))
+
 print(current_date.strftime("%B %d %Y"))
