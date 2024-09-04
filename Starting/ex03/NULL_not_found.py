64cf3c9c17c9b10d00d49985394ff5a5950b4b55
Alex Lamizana <alamizan@1C1.42angouleme.fr>
2024-09-04 11:10:09 +0200

    ex03 fini

--- Starting/ex03/NULL_not_found.py
+++ Starting/ex03/NULL_not_found.py
@@ -1,12 +1,35 @@
-
-
-
 def NULL_not_found(object: any) -> int:
+    """ imprime le type d'objet de tous les types de Null."""
 
     if object is None:
-        print(f"Nothing: {object}: {type(object)}")
+        print(f"Nothing: {object} {type(object)}")
+        return(0)
+    
+    elif isinstance(object, bool):
+        print(f"Fake: {object} {type(object)}")
+        return(0)
+    
+    elif isinstance(object, float):
+        print(f"Cheese: {object} {type(object)}")
+        return(0)
+    
+    elif isinstance(object, int):
+        print(f"Zero: {object} {type(object)}")
         return(0)
 
+    elif isinstance(object, str):
+        if object is not Empty:
+            print("Type not Found")
+            return(1)
+        print(f"Empty: {object} {type(object)}")
+        return(0)
+    
+    else:
+        print("Type not found")
+    
+    return(1)
+
+#-----------------------------------------------------------------------------#
 
 Nothing = None
 Garlic = float("NaN")
@@ -18,7 +41,7 @@ NULL_not_found(Garlic)
 NULL_not_found(Zero)
 NULL_not_found(Empty)
 NULL_not_found(Fake)
-# print(NULL_not_found("Brian"))
+print(NULL_not_found("Brian"))
 
 
 # Nothing: None <class 'NoneType'>$
