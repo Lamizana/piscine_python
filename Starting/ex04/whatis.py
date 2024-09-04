4d80922e4b29c71ed1dac6ea950761bdd1be2956
Alex Lamizana <alamizan@1C1.42angouleme.fr>
2024-09-04 14:54:38 +0200

    ex04 finit

---
+++ Starting/ex04/whatis.py
@@ -0,0 +1,15 @@
+import sys
+
+try:
+    assert (len(sys.argv) == 2 and sys.argv[1] != ""), 'AssertionError: more than one argument is provided'
+    assert (sys.argv[1][0] == '-' or sys.argv[1][0] == '+' or sys.argv[1][0].isdigit()), 'AssertionError: argument is not an integer'
+    assert sys.argv[1][1:].isdigit(), 'AssertionError: argument is not an integer'
+except AssertionError as msg:
+    print(msg)
+    sys.exit(1)
+
+int_num = int(sys.argv[1])
+if (int_num % 2 == 0):
+    print("I'm Even.")
+else:
+    print("I'm Odd.")
