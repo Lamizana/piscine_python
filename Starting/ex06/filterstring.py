9d3cbe6117ab1cd0915842317a13c688c0b876fe
Alex Lamizana <alamizan@1E5.42angouleme.fr>
2024-09-09 11:12:30 +0200

    push ex06

--- Starting/ex06/filterstring.py
+++ Starting/ex06/filterstring.py
@@ -2,73 +2,53 @@
 #######################################################################
 # Importations de fonctions externes :
 import sys
-from typing import Any, Callable, Generator, Iterable, T
+
 #######################################################################
 # Definitions locales de fonctions :
-s_red = "\033[4;31m"
-white = "\033[0m"
 
 
 def check_argv() -> None:
     """Verifie le nombre d'arguments (2 requis) ainsi que leur validite"""
 
-    msg = f"{s_red}AssertionError: the arguments are bad{white}"
+    msg = "AssertionError: the arguments are bad"
     try:
         assert (len(sys.argv) == 3), msg
-        assert not isinstance(sys.argv[2], (int)), msg
+        int(sys.argv[2].strip())
     except AssertionError as msg:
+        print(msg)
+        sys.exit(1)
+    except ValueError:
         print(msg)
         sys.exit(1)
 
 
-
-
-def my_filter(func: Callable[[T], Any], iterable: Iterable[T]) -> Generator[T, None, None]:
-    """Yields all values from the iterable for which the function returns a truthful value"""
-    
-    for value in iterable:
-        if func(value):
-            yield value
-
 def ft_filter(func, sequence):
-    result = []
-    for i in sequence:
-        if func(i) is True:
-            result.append(i)
-    return result
+    """ Filtre une sequence en fonction de la fonction donnee"""
+
+    if (func is None):
+        yield (x for x in sequence if x)
+    else:
+        yield (x for x in sequence if func(x) is True)
+
 
 
 def main() -> int:
     """Fonction programme principal"""
 
     check_argv()
+    list_argv = (sys.argv[1].split(" "))
+    len_word = int(sys.argv[2])
+    print(f"Arguments listes:\n{list_argv}")
+    print(f"Longueur des mots demande: {len_word}")
+    result = (ft_filter(lambda word: len(word) > len_word, list_argv))
+    result2 = (filter(lambda word: len(word) > len_word, list_argv))
 
-    personnes = [18, 23, 42, 61, 88]
+    print((result))
+    print((result2))
 
-    # travailleurs = filter(lambda age: age <= 62, personnes)
-    titi = my_filter(lambda age: age <= 62, personnes)
-    toto = ft_filter(lambda age: age <= 62, personnes)
-    
-    
-
-    n = 2
-    t = (sys.argv[1].split(" "))
-    print(t)
-    words = filter(lambda word: len(word) > 4, t)
-    words2 = [word for word in t if len(word) < 2]
-    
-    # print(list(travailleurs))
-    print(list(titi))
-    print(list(toto))
-    
-    
-    print(list(words))
-    print(list(words2))
-    
 
 
 #######################################################################
 # Corps principal du programme :
 if __name__ == "__main__":
     sys.exit(main())
-
--- Starting/ex06/ft_filter.py
+++ Starting/ex06/ft_filter.py
@@ -1,16 +1,15 @@
-from typing import Any, Callable, Generator, Iterable, T
-
-def my_filter(func: Callable[[T], Any], iterable: Iterable[T]) -> Generator[T, None, None]:
-    """Yields all values from the iterable for which the function returns a truthful value"""
-    
-    for value in iterable:
-        if func(value):
-            yield value
-            
-            
 def ft_filter(func, sequence):
-    result = []
-    for i in sequence:
-        if func(i) is True:
-            result.append(i)
-        return result
+    """ Filtre une sequence en fonction de la fonction donnee"""
+
+    if (func is None):
+        yield (x for x in sequence if x)
+    else:
+        yield (x for x in sequence if func(x) is True)
+
+
+
+
+
+
+
+    
